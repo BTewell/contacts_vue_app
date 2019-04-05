3ab6da86f75095cbc646fdea5d79395e37d54a59
“Brett <bretttewell@gmail.com>
2019-04-05 15:49:35 -0500

    Vue.js exercises, initial commit, Vue.js set up, index action create.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <hr>
+    <div v-for="contact in contacts">
+      <p>{{ contact.last_name }}{{", "}}{{ contact.first_name }}</p>
+    </div>
   </div>
 </template>
 
+<style>
+  img {
+    width: 200px;
+  }
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Welcome to your Contacts Application!",
+      contacts: [],
+    };
+  },
+  created: function() {
+    axios.get("/api/contacts").then(response => {
+      this.contacts = response.data;
+    });
+  },
+  methods: {}
+};
 </script>
